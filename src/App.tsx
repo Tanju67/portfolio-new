5da01f420a9e13fd7de6784dd5297e23b0eec3bf
Tanju Özer <tanju.ozer64@gmail.com>
2026-05-07 18:40:28 +0200

    create routing and implement lazy loading for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,63 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import ErrorPage from "./pages/ErrorPage";
+import RootLayout from "./pages/RootLayout";
+const LandingPage = lazy(() => import("./pages/LandingPage"));
+const ProjectsPage = lazy(() => import("./pages/ProjectsPage"));
+const SkillsPage = lazy(() => import("./pages/SkillsPage"));
+const AboutPage = lazy(() => import("./pages/AboutPage"));
+const ContactPage = lazy(() => import("./pages/ContactPage"));
+const ProjectDetailsPage = lazy(() => import("./pages/ProjectDetailsPage"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "projects",
+        children: [
+          {
+            index: true,
+            element: <ProjectsPage />,
+          },
+          {
+            path: ":id",
+            element: <ProjectDetailsPage />,
+          },
+        ],
+      },
+      {
+        path: "skills",
+        element: <SkillsPage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Suspense fallback={null}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 }
 
 export default App;
